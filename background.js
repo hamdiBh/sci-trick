020b7fa0abd78f29ddcd83718c34047c214f265d
khalilcharfi <khalilcharfi8@gmail.com>
2016-01-17 20:48:14 +0100

    Update

--- background.js
+++ background.js
@@ -1,9 +1,29 @@
 // Copyright (c) 2016 H@mdi.
 
+if( storage == null){
+var storage = chrome.storage.local;
 
+var state = 'state';
+
+var obj= {};
+
+obj[state] = true;
+
+storage.set(obj);
+
+storage.get(state,function(result){
+  console.log('hamdi');
+  //console output = myVariableKeyName {myTestVar:'my test var'}
+});}
+
+
+storage.get(state,function(result){
+  if(result.state == true){
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+
+
   console.log('bareUrl' + tab.url);
- 	origin_url = tab.url;
+  origin_url = tab.url;
   var arr = origin_url.split('/');
     if((arr[2].indexOf('ieeexplore.ieee') >= 0
       || arr[2].indexOf('link.springer')>=0
@@ -19,15 +39,28 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
       chrome.tabs.update(tab.id, {url: new_url});
 
     }
+});}
 });
 
 
+
 chrome.browserAction.onClicked.addListener(function(tab) {
-  var arr = tab.url.split('/');
-  arr[2] = arr[2] + ".sci-hub.io";
-  var new_url = arr.join('/');
 
-  console.log('Redirecting ' + tab.url + ' to ' + new_url);
+storage.get(state,function(result){
+  if(result.state == true){
+chrome.browserAction.setIcon({path:"disabled.png"});
+obj[state] = false;
 
-  chrome.tabs.update(tab.id, {url: new_url});
+storage.set(obj);
+
+}else{
+obj[state] = true;
+
+storage.set(obj);
+chrome.browserAction.setIcon({path:"enabled.png"});
+}
+
+   
+});
+
 });
