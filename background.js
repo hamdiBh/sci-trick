74a3cf8bcbb91a560bc2edb820e6d3dc16ab5327
khalilcharfi <khalilcharfi8@gmail.com>
2016-02-12 10:08:44 +0100

    Update

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 // Copyright (c) 2016 H@mdi & KC.
-var Website = ["ieeexplore.iee", "link.springer", "sciencedirect", "dl.acm", "onlinelibrary.wiley", "jama.jamanetwork", "cpr.sagepub", "cnu.sagepub.com", "ehjcimaging.oxfordjournals", "europace.oxfordjournals.org", "tandfonline.com", "biomet.oxfordjournals", "online.liebertpub", "nejm.org"];
+var Website = ["ieeexplore.iee", "link.springer", "sciencedirect", "dl.acm", "onlinelibrary.wiley", "jama.jamanetwork", "cpr.sagepub", "cnu.sagepub.com", "ehjcimaging.oxfordjournals", "europace.oxfordjournals.org", "tandfonline.com", "biomet.oxfordjournals", "online.liebertpub", "nejm.org", "ptjournal.apta.org"];
 var Button_state = ["Sci-trick [enabled]", "Sci-trick [disabled]", "icon/enabled.png", "icon/disabled.png"]
 
 function CheckWebsite(tab, changeInfo)
